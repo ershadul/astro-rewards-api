17812a348a811e644d650370551997327592aa1d
Ershadul Hoque <ershadulhoque@gmail.com>
2019-07-03 19:19:52 +0600

    Make reward id require in prmocode

--- server/promocode/index.js
+++ server/promocode/index.js
@@ -1,14 +1,22 @@
 const Subscriber = require('../subscriber/subscriber');
+const Reward = require('../reward/reward');
 const Promocode = require('./promocode');
 
 function create(req, res) {
   const data = req.body || {};
   data.tenant = req.tenant.id;
 
-  Subscriber.findOne(data, (err, subscriber) => {
-    if (err) { return res.status(400).json(err); }
-    if (!subscriber) { return res.status(400).json({ message: 'Invalid subscriber id provided.' }); }
-    Promocode.create({ subscriber: subscriber.id }, (err2, promocode) => {
+  Promise.all([
+    Subscriber.findOne({
+      tenant: req.tenant.id,
+      idType: req.body.idType,
+      idNumber: req.body.idNumber,
+      accountNumber: req.body.accountNumber,
+    }).exec(),
+    Reward.findById(req.body.reward).exec(),
+  ]).then((result) => {
+    if (!result[0] || !result[1]) { return res.status(400).json({ message: 'Invalid subscriber info provided.' }); }
+    Promocode.create({ subscriber: result[0].id, reward: result[1].id }, (err2, promocode) => {
       if (err2) { return res.status(400).json(err2); }
       return res.status(201).send(promocode);
     });
--- server/promocode/promocode.js
+++ server/promocode/promocode.js
@@ -7,10 +7,10 @@ const { Schema } = mongoose;
 const PromocodeSchema = new Schema({
   subscriber: { type: Schema.Types.ObjectId, ref: 'Subscriber', required: true },
   code: { type: String, trim: true, default: uuidv4 },
+  reward: { type: Schema.Types.ObjectId, ref: 'Reward', required: true },
 });
 
 PromocodeSchema.plugin(timestamp);
 
-// Export the model
 const Promocode = mongoose.model('Promocode', PromocodeSchema);
 module.exports = Promocode;
